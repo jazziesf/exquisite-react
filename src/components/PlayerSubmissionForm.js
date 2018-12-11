6e6fd632de7ff6795de918d21b9559f57b328b6c
Jazz <jazziesf21@yahoo.com>
2018-12-11 14:48:41 -0800

    form created, css sytling applied

--- src/components/PlayerSubmissionForm.js
+++ src/components/PlayerSubmissionForm.js
@@ -5,24 +5,91 @@ class PlayerSubmissionForm extends Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      adj1: "",
+      adj2: "",
+      adv: "" ,
+      noun1: "",
+      noun2: "",
+      verb: "",
+      }
+    }
+
+  onFiledChangeHandler = (event, fieldToGetValueFrom = "value") => {
+    const updateState = {};
+
+    const fieldName = event.target.name;
+    const value = event.target[fieldToGetValueFrom];
+
+    updateState[fieldName] = value;
+
+    this.setState(updateState);
   }
 
   render() {
 
     return (
-      <div className="PlayerSubmissionForm">
+      <div className="PlayerSubmissionForm__form">
         <h3>Player Submission Form for Player #{  }</h3>
 
         <form className="PlayerSubmissionForm__form" >
 
           <div className="PlayerSubmissionForm__poem-inputs">
-
-            {
-              // Put your form inputs here... We've put in one below as an example
-            }
+            <p>The</p>
             <input
-              placeholder="hm..."
-              type="text" />
+              name="adj1"
+              placeholder="adjective"
+              type="text"
+              className={this.state.adj1.length < 1 ? "PlayerSubmissionFormt__input--invalid" : "PlayerSubmissionFormt__input" }
+              value={this.state.adj1}
+              onChange={this.onFiledChangeHandler}
+              />
+
+            <input
+              name="noun1"
+              placeholder="noun"
+              type="text"
+              className={this.state.noun1.length < 1 ? "PlayerSubmissionFormt__input--invalid" : "PlayerSubmissionFormt__input" }
+              value={this.state.noun1}
+              onChange={this.onFiledChangeHandler}
+              />
+
+            <input
+              placeholder="adverb"
+              name="adv"
+              type="text"
+              className={this.state.adv.length < 1 ? "PlayerSubmissionFormt__input--invalid" : "PlayerSubmissionFormt__input" }
+              value={this.state.adv}
+              onChange={this.onFiledChangeHandler}
+              />
+
+            <input
+              name="verb"
+              placeholder="verb"
+              type="text"
+              className={this.state.verb.length < 1 ? "PlayerSubmissionFormt__input--invalid" : "PlayerSubmissionFormt__input" }
+              value={this.state.verb}
+              onChange={this.onFiledChangeHandler} />
+
+            <p>the</p>
+
+            <input
+              name="adj2"
+              placeholder="adjective"
+              type="text"
+              className={this.state.adj2.length < 1 ? "PlayerSubmissionFormt__input--invalid" : "PlayerSubmissionFormt__input" }
+              value={this.state.adj2}
+              onChange={this.onFiledChangeHandler}
+              />
+
+            <input
+              name="noun2"
+              placeholder="noun"
+              type="text"
+              className={this.state.noun2.length < 1 ? "PlayerSubmissionFormt__input--invalid" : "PlayerSubmissionFormt__input" }
+              value={this.state.noun2}
+              onChange={this.onFiledChangeHandler}
+              />
 
           </div>
 
@@ -34,5 +101,10 @@ class PlayerSubmissionForm extends Component {
     );
   }
 }
+
+// PlayerSubmissionForm.propTypes = {
+//   addLineSubmission: PropTypes.func.isRequired
+// }
+
 
 export default PlayerSubmissionForm;
